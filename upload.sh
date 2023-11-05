6609a82b5bdffe17d10582eba0565cd25ae7458b
MaxMax6767 <maxime.gug@gmail.com>
2023-11-05 20:30:15 +0100

    update upload.sh

--- upload.sh
+++ upload.sh
@@ -13,7 +13,11 @@ file_name=$(basename "$1")
 avr-objcopy -O ihex "$1" "$1.hex"
 
 # Téléverse le fichier `.hex` sur la carte Arduino
-avrdude -p atmega328p -c arduino -P /dev/ttyACM0 -b 115200 -U flash:w:"$1.hex":i
+avrdude -p atmega328p -c arduino -P /dev/ttyACM0 -b 115200 -U flash:w:"$1.hex":i -v
+
+# Supprime le fichier `.hex`
+rm "$1.hex"
 
 # Affiche un message de réussite
 echo "Téléversement réussi."
+
